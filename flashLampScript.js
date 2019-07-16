f4460836eabc9107574d08b93149324b54f9f898
hungavcodegym <anhung94@gmail.com>
2019-07-16 10:57:09 +0700

    update

--- flashLampScript.js
+++ flashLampScript.js
@@ -1,15 +1,16 @@
-function FlashLamp(status, battery) {
-    this.status = status;
-    this.battery = battery;
+function FlashLamp() {
+    this.status = false;
+    this.battery = Battery;
     this.setBattery = function (battery) {
         this.battery = battery;
     };
     this.getBatteryInfo = function () {
-        return this.battery;
+        return this.battery.getEnergy();
     };
     this.light = function () {
         if (this.status) {
             alert('Lighting');
+            this.battery.decreaseEnergy();
         } else {
             alert('Not lighting')
         }
